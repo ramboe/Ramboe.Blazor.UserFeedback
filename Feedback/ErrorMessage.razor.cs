32c7b75d67ccfe3d37086a0dc21db376f60d7d91
ckruse <cle.kruse@hotmail.com>
2023-01-28 11:41:33 +0100

    cleanups

--- Feedback/ErrorMessage.razor.cs
+++ Feedback/ErrorMessage.razor.cs
@@ -5,35 +5,8 @@ namespace Ramboe.Blazor.UserFeedback.Feedback;
 
 public partial class ErrorMessage
 {
-    #region injections
-    #endregion
-
     #region params
-    [Parameter] public string StatusText { get; set; }
+    [Parameter] public string Text { get; set; }
     [Parameter] public BlazorExceptionModel Status { get; set; }
-
-    [Parameter] public string MaxWith { get; set; }
-
-    [Parameter] public string MarginBottom { get; set; }
-
-    [Parameter] public string MarginTop { get; set; }
     #endregion
-
-    #region props
-    string _marginbottom = string.Empty;
-    string _margintop = string.Empty;
-    #endregion
-
-    protected override void OnInitialized()
-    {
-        if (!string.IsNullOrEmpty(MarginBottom))
-        {
-            _marginbottom = $"margin-bottom: {MarginBottom};";
-        }
-
-        if (!string.IsNullOrEmpty(MarginTop))
-        {
-            _margintop = $"margin-bottom: {MarginTop};";
-        }
-    }
 }
